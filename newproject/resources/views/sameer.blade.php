2888b14901be1f407aa39582083c2dfab43035e5
sameerK-992 <sameer2.smartitventures@gmail.com>
2019-11-22 16:14:56 +0530

    filepush

---
+++ newproject/resources/views/sameer.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('resources/css/style.css') }}">
+</head>
+<body>
+
+<div class="heading">
+<p>Hello this is my first page </p>
+</div>
+
+
+
+</body>
+</html>
